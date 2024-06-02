b74bb0f55dd6642ecc2e70ae36d5ad86b59cd903
Anish202020 <anishkumarbdmi@gmail.com>
2024-06-02 21:43:01 +0530

    commit changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
           <Navbar />
           <main className="w-full h-full flex flex-col md:flex-row">
             <Sidebar />
-            {children}
+            <div className="w-full overflow-auto md:pr-32">{children}</div>
           </main>
         </body>
       </UserProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,42 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 export default function Home() {
   const { user, error, isLoading } = useUser();
   return (
-    <main>
-      <h2>Hello</h2>
-      <p>The user {user?.name}</p>
-      {user && <p>The user {user?.name} is logged in.</p>}
-
+    <section className="w-full flex flex-col">
       {user ? (
-        <a href={"/api/auth/logout"}>Logout</a>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="mt-4 text-4xl font-bold text-center text-indigo-600">
+            Hi, {user?.nickname || user?.name || "dear user"}!
+          </h1>
+          <h2 className="text-xl max-w-lg text-center text-gray-600">
+            Welcome to Bloggify, where you can easily create full blog posts
+            with just one click!
+          </h2>
+          <Link
+            href="/new"
+            className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-500 transition-all
+              cursor-pointer"
+          >
+            Get Started
+          </Link>
+        </div>
       ) : (
-        <a href={"/api/auth/login"}>Login</a>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="mt-4 text-4xl font-bold text-center text-indigo-600">
+            Hello!
+          </h1>
+          <h2 className="text-xl max-w-lg text-center text-gray-600">
+            Welcome to Bloggify, where you can easily create full blog posts
+            with just one click!
+          </h2>
+          <a
+            href="/api/auth/login"
+            className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-500 transition-all
+                  cursor-pointer"
+          >
+            Login to get started
+          </a>
+        </div>
       )}
-      <div>
-        <Link href={"/profile"}>User</Link>
-      </div>
-    </main>
+    </section>
   );
 }
