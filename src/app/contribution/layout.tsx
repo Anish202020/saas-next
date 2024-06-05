051edde34d6466b225d5a6f40dcd8b0afccad9b9
Anish202020 <anishkumarbdmi@gmail.com>
2024-06-05 06:40:49 +0530

    commit changes

---
+++ src/app/contribution/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+export const metadata = {
+  title: "Contribution | AI-Bloggy",
+  description:
+    "NextJS template with TypeScript, TailwindCSS, and MongoDB, created by @clipper.",
+};
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default layout;
