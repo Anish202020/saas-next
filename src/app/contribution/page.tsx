c448884cfac81aca532371d23c82300c97f5f5a9
Anish202020 <anishkumarbdmi@gmail.com>
2024-06-13 19:39:33 +0530

    commit changes

--- src/app/contribution/page.tsx
+++ src/app/contribution/page.tsx
@@ -7,19 +7,19 @@ export default withPageAuthRequired(function Page() {
   return (
     <div>
       <div className="text-gray-600 picture body-font">
-        <div className="container px-5 py-1 mx-auto">
+        <div className="container px-5  mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
             {/* <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-blue-600">Our Team</h1> */}
             <h1 className="text-4xl font-bold mt-4 text-blue-600">Our Team</h1>
-            <p className="lg:w-2/3 mx-auto py-10 leading-relaxed text-base">
+            <p className="lg:w-2/3 mx-auto py-10 text-gray-800 font-semibold leading-relaxed text-base">
               Together, our diverse skills and unity can conquer any challenge.
               We have collaborate, support, and achieve greatness in our
               project!
             </p>
           </div>
           <div className="flex flex-wrap -m-2">
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+            <div className="p-2 lg:w-1/3  md:w-1/2 w-full">
+              <div className="h-full flex bg-gray-50 items-center border-gray-200 border p-4 rounded-lg">
                 <Image
                   alt="team"
                   width={100}
@@ -36,7 +36,7 @@ export default withPageAuthRequired(function Page() {
               </div>
             </div>
             <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+              <div className="h-full flex bg-gray-50 items-center border-gray-200 border p-4 rounded-lg">
                 <Image
                   alt="team"
                   width={100}
@@ -53,7 +53,7 @@ export default withPageAuthRequired(function Page() {
               </div>
             </div>
             <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+              <div className="h-full flex bg-gray-50 items-center border-gray-200 border p-4 rounded-lg">
                 <Image
                   alt="team"
                   width={100}
