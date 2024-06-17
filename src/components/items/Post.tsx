7ef3ee6e163df2ab2507ca6c6871471b95a48a54
Anish202020 <anishkumarbdmi@gmail.com>
2024-06-17 18:27:02 +0530

    commit changes

--- src/components/items/Post.tsx
+++ src/components/items/Post.tsx
@@ -1,22 +1,46 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import { FaCopy } from "react-icons/fa";
 import { TiDocumentDelete } from "react-icons/ti";
 import { MdDeleteForever } from "react-icons/md";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface Props {
   post: PostWithId;
   handleDeletePost: (_id: string) => void;
 }
-export default function Post({post,handleDeletePost}:Props) {
-    function handleCopyToClipboard() {
-        navigator.clipboard.writeText(post.title + "\n" + post.content);
-      }
-      function handleDelete() {
-        handleDeletePost(post._id);
-      }
+export default function Post({ post, handleDeletePost }: Props) {
+  function handleCopyToClipboard() {
+    navigator.clipboard.writeText(post.title + "\n" + post.content);
+    toast.info("Copied the Post!", {
+      position: "bottom-left",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      // transition: Bounce,
+    });
+  }
+  function handleDelete() {
+    toast.info("Deleted the Post!", {
+      position: "bottom-left",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      // transition: Bounce,
+    });
+    handleDeletePost(post._id);
+  }
   return (
-    <div className='w-full flex flex-col gap-4 shadow-sm p-4 rounded-xl bg-white'>
+    <div className="w-full flex flex-col gap-4 shadow-sm p-4 rounded-xl bg-white">
       <h1 className="text-2xl px-4 font-bold text-center text-gray-800">
         {post.title}
       </h1>
@@ -36,15 +60,43 @@ export default function Post({post,handleDeletePost}:Props) {
                 cursor-pointer"
         onClick={handleCopyToClipboard}
       >
-        <FaCopy className='mt-1'/>Copy the Post
+        <FaCopy className="mt-1" />
+        Copy the Post
+        <ToastContainer
+          position="bottom-left"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          // transition: Bounce,
+        />
       </button>
       <button
         className="bg-red-600 flex gap-1 justify-center text-white px-4 py-2 rounded-md hover:bg-red-500 transition-all
                 cursor-pointer"
         onClick={handleDelete}
       >
-        <MdDeleteForever className='text-xl mt-0.5'/>Delete the Post
+        <MdDeleteForever className="text-xl mt-0.5" />
+        Delete the Post
+        <ToastContainer
+          position="bottom-left"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          // transition: Bounce,
+        />
       </button>
     </div>
-  )
+  );
 }
